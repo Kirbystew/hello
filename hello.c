1462b0a99e95e0280a730b323372508c44d4ba9f
Kirbystew <142818059+Kirbystew@users.noreply.github.com>
2023-08-24 11:14:11 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Gavin Lane
+ *         EMAIL
+ * Date: 8/22/2023
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Gavin Lane!\n");
+  return 0;
+}
